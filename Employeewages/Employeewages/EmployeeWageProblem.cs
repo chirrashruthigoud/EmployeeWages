79cfe2c3ad263ce2649b7cdcf18b902715675016
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-22 15:00:27 +0530

    Add : Employee Daily Wages

--- Employeewages/Employeewages/EmployeeWageProblem.cs
+++ Employeewages/Employeewages/EmployeeWageProblem.cs
@@ -7,21 +7,25 @@ using System.Threading.Tasks;
 namespace Employeewages
 {
     public class EmployeeWageProblem
-    {
+       {
+            int FULL_TIME = 1;
+            int EmpHrs = 0;
+            int empWage = 0;
+            int EmpRateHrs = 20;
         public void CheckEmployee()
         {
-            int FULL_TIME = 1;
-            Random random = new Random();
-            int empCheck = random.Next(2);
-
-            if (empCheck == FULL_TIME)
-            {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
+           Random random = new Random();
+           int empCheck = random.Next(2);
+                if (empCheck == FULL_TIME)
+                {
+                    EmpHrs = 8;
+                }
+                else
+                {
+                    EmpHrs = 0;
+                }
+             empWage = EmpHrs + EmpRateHrs;
+             Console.WriteLine($"Employee Wage is : {empWage}");
         }
     }
 }
